497b505ea1c5cf5eda45e603b4c08a253cd455b0
alexaaqil <alexaaqil.se@gmail.com>
2025-05-19 17:39:41 +0300

    added uuid to the user schema.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('uuid')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('phone_number')->nullable();
+            $table->string('secondary_phone_number')->nullable();
+            $table->string('avatar')->nullable();
+            $table->unsignedTinyInteger('role')->default(3);
+            $table->unsignedTinyInteger('status')->default(1);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
