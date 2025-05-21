ed07961887ac0a4bdd45ba8d43f605aec83923db
alexaaqil <alexaaqil.se@gmail.com>
2025-05-21 22:46:37 +0300

    added the profile update functionality.

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Pages\Dashboard\Index as Dashboard;
 use App\Livewire\Pages\General\Index as HomePage;
+use App\Livewire\Auth\Profile\Index as Profile;
 
 Route::get('/', HomePage::class)->name('home-page');
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('profile', Profile::class)->name('profile.edit');
+});
 
 Route::middleware(['auth', 'verified'])->group(function() {
     Route::get('dashboard', Dashboard::class)->name('dashboard');
