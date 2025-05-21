7ed6a119f796cc4bc05d6454b454e1ac6f4d6059
alexaaqil <alexaaqil.se@gmail.com>
2025-05-21 20:25:37 +0300

    added the functionality for dynamically updating the full name of the user on the navbar.

--- app/Livewire/Partials/AppNavbar.php
+++ app/Livewire/Partials/AppNavbar.php
@@ -4,9 +4,26 @@ namespace App\Livewire\Partials;
 
 use Livewire\Component;
 use App\Livewire\Actions\Auth\Logout;
+use Illuminate\Support\Facades\Auth;
 
 class AppNavbar extends Component
 {
+    public string $name = '';
+
+    protected $listeners = [
+        'profile-updated' => 'refreshName'
+    ];
+
+    public function mount(): void
+    {
+        $this->refreshName();
+    }
+
+    public function refreshName($name = null)
+    {
+        $this->name = $name ?? Auth::user()->full_name;
+    }
+
     public function logout(Logout $logout)
     {
         $logout();
--- resources/views/livewire/partials/app-navbar.blade.php
+++ resources/views/livewire/partials/app-navbar.blade.php
@@ -38,7 +38,7 @@
         @auth
             <div class="user_dropdown hidden md:block" x-data="{ open: false }" @click.outside="open = false">
                 <button @click="open = !open" class="user_dropdown_toggle">
-                    <span>{{ Auth::user()->name }}</span>
+                    <span>{{ $name }}</span>
                     <svg class="caret" :class="{ 'rotate-180': open }" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
                     </svg>
