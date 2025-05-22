d58bf8f8ea773ba796b8e005c81be9a99d022bb4
alexaaqil <alexaaqil.se@gmail.com>
2025-05-23 01:09:59 +0300

    added some stats and styling to the dashboard.

--- app/Livewire/Pages/Dashboard/Index.php
+++ app/Livewire/Pages/Dashboard/Index.php
@@ -3,11 +3,25 @@
 namespace App\Livewire\Pages\Dashboard;
 
 use Livewire\Component;
+use App\Models\ContactMessage;
+use App\Models\User;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.pages.dashboard.index');
+        $count_users = User::where('role', '>=', '3')->count();
+        $count_admins = User::where('role', '<', '3')->count();
+
+        $count_messages = ContactMessage::count();
+        $count_unread_messages = ContactMessage::where('is_read', false)->count();
+
+        return view('livewire.pages.dashboard.index', compact(
+            'count_admins',
+            'count_users',
+
+            'count_messages',
+            'count_unread_messages',
+        ));
     }
 }
--- resources/views/livewire/pages/dashboard/index.blade.php
+++ resources/views/livewire/pages/dashboard/index.blade.php
@@ -1,4 +1,15 @@
-<div>
-    <p>Hi, {{ Auth::user()->full_name }}</p>
-    <p>You're logged in!</p>
+<div class="Dashboard">
+    <div class="container">
+        <section class="stats">
+            <div class="stat">
+                <span>{{ $count_users }}</span>
+                <span>{{ Str::plural('user', $count_users) }} and {{ $count_admins }} {{ Str::plural('admin', $count_admins) }}</span>
+            </div>
+
+            <div class="stat">
+                <span>{{ $count_messages }}</span>
+                <span>{{ Str::plural('message', $count_messages) }} and {{ $count_unread_messages }} unread</span>
+            </div>
+        </section>
+    </div>
 </div>
